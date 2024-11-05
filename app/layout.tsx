a81f454ff6525369dd1bf90450617eec044f2236
sexfrance <bwuuuuu@gmail.com>
2024-11-05 20:22:24 +0100

    Fixed ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,26 @@ export default function RootLayout({
               </div>
             </div>
           </div>
+          <div className="flex justify-center mt-5">
+            <script
+              type="text/javascript"
+              dangerouslySetInnerHTML={{
+                __html: `
+                  atOptions = {
+                    'key' : '2aa5dd87245064af79eff4d487110df0',
+                    'format' : 'iframe',
+                    'height' : 90,
+                    'width' : 728,
+                    'params' : {}
+                  };
+                `,
+              }}
+            ></script>
+            <script
+              type="text/javascript"
+              src="//www.highperformanceformat.com/2aa5dd87245064af79eff4d487110df0/invoke.js"
+            ></script>
+          </div>
         </ThemeProvider>
       </body>
     </html>
