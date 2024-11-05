b99eea26921b7413dc8413b07caa9c5c93e20a87
sexfrance <bwuuuuu@gmail.com>
2024-11-05 19:59:57 +0100

    Added more ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,27 +18,29 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-          <div className="flex justify-center items-start min-h-screen">
+          <div className="flex justify-center items-start min-h-screen relative">
             {/* Left Ad Container */}
-            <div className="flex-none w-40 h-full">
-              <script
-                type="text/javascript"
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    atOptions = {
-                      'key' : 'ca77b3919db22954fff14719a4e43fcb',
-                      'format' : 'iframe',
-                      'height' : 600,
-                      'width' : 160,
-                      'params' : {}
-                    };
-                  `,
-                }}
-              ></script>
-              <script
-                type="text/javascript"
-                src="//www.highperformanceformat.com/ca77b3919db22954fff14719a4e43fcb/invoke.js"
-              ></script>
+            <div className="fixed left-0 top-0 h-screen flex items-center">
+              <div className="w-40 h-full">
+                <script
+                  type="text/javascript"
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                      atOptions = {
+                        'key' : 'ca77b3919db22954fff14719a4e43fcb',
+                        'format' : 'iframe',
+                        'height' : 600,
+                        'width' : 160,
+                        'params' : {}
+                      };
+                    `,
+                  }}
+                ></script>
+                <script
+                  type="text/javascript"
+                  src="//www.highperformanceformat.com/ca77b3919db22954fff14719a4e43fcb/invoke.js"
+                ></script>
+              </div>
             </div>
 
             {/* Main Content */}
@@ -47,25 +49,27 @@ export default function RootLayout({
             </div>
 
             {/* Right Ad Container */}
-            <div className="flex-none w-40 h-full">
-              <script
-                type="text/javascript"
-                dangerouslySetInnerHTML={{
-                  __html: `
-                    atOptions = {
-                      'key' : 'ca77b3919db22954fff14719a4e43fcb',
-                      'format' : 'iframe',
-                      'height' : 600,
-                      'width' : 160,
-                      'params' : {}
-                    };
-                  `,
-                }}
-              ></script>
-              <script
-                type="text/javascript"
-                src="//www.highperformanceformat.com/ca77b3919db22954fff14719a4e43fcb/invoke.js"
-              ></script>
+            <div className="fixed right-0 top-0 h-screen flex items-center">
+              <div className="w-40 h-full">
+                <script
+                  type="text/javascript"
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                      atOptions = {
+                        'key' : 'ca77b3919db22954fff14719a4e43fcb',
+                        'format' : 'iframe',
+                        'height' : 600,
+                        'width' : 160,
+                        'params' : {}
+                      };
+                    `,
+                  }}
+                ></script>
+                <script
+                  type="text/javascript"
+                  src="//www.highperformanceformat.com/ca77b3919db22954fff14719a4e43fcb/invoke.js"
+                ></script>
+              </div>
             </div>
           </div>
         </ThemeProvider>
